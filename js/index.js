842d229246923786875222f69a5e1e3afe23067a
PixelGenetics <antoniokevin153@outlook.com>
2023-05-13 18:54:06 -0600

    Creando logica para obtener colores al azar y lineal

---
+++ js/index.js
@@ -0,0 +1,64 @@
+const cuerpo = document.querySelector("body");
+const display = document.querySelector("p");
+let cont = 0;
+
+function cambio2(){
+    let color = "#0298FF";
+    cuerpo.style.background = color;
+    mostrar(color);
+}
+
+function cambio0(){
+    let color = "#FF0202"
+    cuerpo.style.background = color;
+    mostrar(color);
+}
+
+function cambio1(){
+    let color = "#02FF47"; 
+    cuerpo.style.background = color;
+    mostrar(color);
+}
+
+function mostrar(color1){
+    display.innerText = color1;
+}
+
+
+/* ============================================================================== */
+
+
+function coloresRandom(){
+    
+    let color1 = "#FF0202", color2 = "#0298FF", color3 = "#02FF47"
+    
+    cont++;
+    if (cont == 1) {
+        cuerpo.style.background = color1;
+    } 
+    if(cont == 2){
+        cuerpo.style.background = color2;
+    } 
+    if(cont == 3){
+        cuerpo.style.background = color3;
+    } 
+    if(cont >= 4){
+        cont = 0;
+    }
+    
+    //console.log(cont);
+}
+
+
+/* ======================================================================== */
+
+
+let arrayColors = ["#FF0202","#0298FF","#02FF47"];
+
+function colorRandom(){
+    let resultado = Math.floor(Math.random() * arrayColors.length);
+    
+    cuerpo.style.background = arrayColors[resultado];
+} 
+
+
